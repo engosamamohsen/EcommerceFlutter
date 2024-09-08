7809d0d1b8fe70f604fd845c9d3f65fe9289b5f0
omohsentrenddc <omohsen@trenddc.com>
2024-09-08 17:40:30 +0300

    worked in image Round

--- lib/component/image/image_round.dart
+++ lib/component/image/image_round.dart
@@ -1,22 +1,20 @@
+import 'package:flutter/material.dart';
+
 class ImageRound extends StatelessWidget {
-  const ImageRound({super.key, required this.image });
+  const ImageRound({super.key, required this.image, required this.round});
   final String image;
+  final double round;
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-      appBar: AppBar(
-        title: Text('Rounded Image Example'),
-      ),
-      body: Center(
-        child: Container(
-          width: 150,
-          height: 150,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10.0), // Border radius of 10
-            image: DecorationImage(
-              image: NetworkImage('https://via.placeholder.com/150'), // Replace with your image URL
-              fit: BoxFit.cover,
-            ),
+    return SizedBox(
+      width: 150,
+      height: 150,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(round), // Border radius of 10
+          image: DecorationImage(
+            image: NetworkImage(image), // Replace with your image URL
+            fit: BoxFit.cover,
           ),
         ),
       ),
