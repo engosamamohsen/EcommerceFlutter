288e7231d1970e78012c63e529286486fd097428
omohsentrenddc <omohsen@trenddc.com>
2024-10-07 18:37:09 +0300

    worked in routes

---
+++ lib/app/routes/const_routes_url.dart
@@ -0,0 +1,19 @@
+class RouteUrlConst {
+  static String HOME = "/";
+  static String LOGIN = "/login";
+  static String REGISTER = "/register";
+  static String FORGET_PASSWORD = "/forget-password";
+  static String CONFIRM_CODE = "/confirm-code";
+  static String ADD_ADDRESS = "/address/add";
+  static String ADDRESS_LIST = "/address/list";
+  static String CART = "/cart";
+  static String CATEGORIES = "/categories";
+  static String CHECKOUT = "/checkout";
+  static String CONTACT_US = "/contact-us";
+  static String PROFILE = "/profile";
+  static String WISHLIST = "/wishlist";
+  static String PRODUCT_DETAILS = "/product-details/{id}";
+  static String PRODUCT_LIST = "/product/list";
+  static String EDIT_PROFILE = "/profile/edit";
+  static String SPLASH = "/splash";
+}
