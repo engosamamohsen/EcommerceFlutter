7c57bad1086368a2a39e9a036f8fc1d0684a3215
omohsentrenddc <omohsen@trenddc.com>
2024-10-13 17:00:11 +0300

    A

--- lib/db_helper/app_storage.dart
+++ lib/db_helper/app_storage.dart
@@ -1,57 +1,57 @@
 import 'dart:convert';
 
 import 'package:Emend/models/login/login_response.dart';
-// import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class AppStorageShared {
-  // final flutterSecureStorage = new FlutterSecureStorage();
+  final flutterSecureStorage = new FlutterSecureStorage();
 
   String _token = "TOKEN";
   String _user = "USER";
 
   void putToken(value) async {
     print("token for saving $value");
-    // await flutterSecureStorage.write(key: _token, value: value);
-    // String? token = await flutterSecureStorage.read(key: _token);
+    await flutterSecureStorage.write(key: _token, value: value);
+    String? token = await flutterSecureStorage.read(key: _token);
     print("token for saving HERERERER $value");
   }
 
   Future<String?> readToken() async {
-    // String? value = await flutterSecureStorage.read(key: _token);
-    return "value";
+    String? value = await flutterSecureStorage.read(key: _token);
+    return value;
   }
 
   Future<void> removeToken() async {
-    // await flutterSecureStorage.delete(key: _token);
+    await flutterSecureStorage.delete(key: _token);
   }
 
   void putKey(key, value) async {
-    // await flutterSecureStorage.write(key: key, value: value);
+    await flutterSecureStorage.write(key: key, value: value);
   }
 
   Future<String> readKey(key) async {
-    // String? result = await flutterSecureStorage.read(key: key);
-    // return result ?? "";
-    return "";
+    String? result = await flutterSecureStorage.read(key: key);
+    return result ?? "";
+    // return "";
   }
 
   Future<void> removeKey(key) async {
-    // await flutterSecureStorage.delete(key: key);
+    await flutterSecureStorage.delete(key: key);
   }
 
   void putUser(UserResponse userResponse) async {
-    // putToken(userResponse.data?.jwtToken);
-    // await flutterSecureStorage.write(
-    //     key: _user, value: jsonEncode(userResponse));
+    putToken(userResponse.data?.jwtToken);
+    await flutterSecureStorage.write(
+        key: _user, value: jsonEncode(userResponse));
   }
 
   Future<UserResponse> getUser() async {
-    // String? result = await flutterSecureStorage.read(key: _user);
-    // if (result != null && result.isNotEmpty) {
-    //   Map<String, dynamic> userMap = jsonDecode(result);
-    //   UserResponse userResponse = UserResponse.fromJson(userMap);
-    //   return userResponse;
-    // }
+    String? result = await flutterSecureStorage.read(key: _user);
+    if (result != null && result.isNotEmpty) {
+      Map<String, dynamic> userMap = jsonDecode(result);
+      UserResponse userResponse = UserResponse.fromJson(userMap);
+      return userResponse;
+    }
     return UserResponse();
   }
 }
